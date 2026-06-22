a6e2147d89b8ac32394b2a0c07558be12ffeca79
Anurag Chauhan <send2anurag.chauhan@gmail.com>
2026-06-22 23:50:50 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ location = each.value.location
 resource "azurerm_storage_account" "this" {
 for_each = var.storage_account
 name = each.value.name
-resource_group_name = each.value.resource_group_name
+resource_group_name = each.value.azurerm_resource_group[this.key].name
 location = each.value.location
 account_tier = each.value.account_tier
 acount_replication_type = each.value.account_replication_type
